70823774dd2c0d1168f2d30be2d157a08ec08e0a
slo-addy <addison.francisco@gmail.com>
2016-09-26 20:36:04 -0700

    First commit

---
+++ Homepwner/ItemsViewController.swift
@@ -0,0 +1,108 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Addison Francisco on 9/25/16.
+//  Copyright © 2016 Addison Francisco. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+    var itemStore: ItemStore!
+    
+    @IBAction func addNewItem(sender: AnyObject) {
+        
+        // Create a new item and add it the the store
+        let newItem = itemStore.createItem()
+        
+        // Figure out where that item is in the array
+        if let index = itemStore.allItems.index(of: newItem) {
+            let indexPath = IndexPath(row: index, section: 0)
+            
+            // Insert this new row into the table
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    @IBAction func toggleEditingMode(sender: AnyObject) {
+        
+        // If you are currently in editing mode...
+        if isEditing {
+            sender.setTitle("Edit", for: .normal)
+            
+            // Turn off editing mode
+            setEditing(false, animated: true)
+        }
+        else {
+            // Change text of button to inform user of state
+            sender.setTitle("Done", for: .normal)
+            
+            // Enter editing mode
+            setEditing(true, animated: true)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemStore.allItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Get a new or recycled cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        
+        // Set the text on the cell with the description of the item
+        // that is at the nth index of items, where n = row this cell
+        // will appear in on the tableview
+        let item = itemStore.allItems[indexPath.row]
+        
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        
+        return cell
+    }
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        // Get the height of the status bar
+        let statusBarHeight = UIApplication.shared.statusBarFrame.height
+        
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        // If the table view is asking to commit a delete command...
+        if editingStyle == .delete {
+            let item = itemStore.allItems[indexPath.row]
+            itemStore.removeItem(item: item)
+            
+            // Also remove that row from the table view with an animation
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        
+        // Update the model
+        itemStore.moveItemAtIndex(fromIndex: sourceIndexPath.row, toIndex: destinationIndexPath.row)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ItemStore.swift
@@ -0,0 +1,44 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by Addison Francisco on 9/25/16.
+//  Copyright © 2016 Addison Francisco. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore {
+    
+    var allItems = [Item]()
+    
+    func createItem() -> Item {
+        let newItem = Item(random: true)
+        
+        allItems.append(newItem)
+        
+        return newItem
+    }
+    
+    func removeItem(item: Item) {
+        if let index = allItems.index(of: item) {
+            allItems.remove(at: index)
+        }
+    }
+    
+    func moveItemAtIndex(fromIndex: Int, toIndex: Int) {
+        
+        if fromIndex == toIndex {
+            return
+        }
+        
+        // Get reference to object being moved so you can reinsert it
+        let movedItem = allItems[fromIndex]
+        
+        // Remove item from array
+        allItems.remove(at: fromIndex)
+        
+        // Insert item in array at new location
+        allItems.insert(movedItem, at: toIndex)
+    }
+}
