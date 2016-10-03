1b90dd06c44628092f76566da4f7febf2a6e3b04
Addison Francisco <addison.francisco@mindbodyonline.com>
2016-10-03 10:38:29 -0700

    Removed unused code that was commented out

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -48,22 +48,6 @@ class ItemsViewController: UITableViewController {
         return itemStore.allItems.count + 1 // Silver Challenge
     }
     
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        
-//        // Get a new or recycled cell
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
-//        
-//        // Set the text on the cell with the description of the item
-//        // that is at the nth index of items, where n = row this cell
-//        // will appear in on the tableview
-//        let item = itemStore.allItems[indexPath.row]
-//        
-//        cell.textLabel?.text = item.name
-//        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
-//        
-//        return cell
-//    }
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Get a new or recycled cell
         let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
