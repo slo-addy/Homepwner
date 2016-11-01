392261bc8db189d6105bd5c325053ad1cc4ca72e
Addison Francisco <addison.francisco@mindbodyonline.com>
2016-10-31 20:07:26 -0700

    Added dynamic type to item cells to account for varying user text sizes. Made changes so that the tableView will update cell labels whenever the user brings the app to the foreground. Also, the valueLabel will now change colors based on price (Bronze Challeng).

---
+++ Homepwner/ItemCell.swift
@@ -0,0 +1,25 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Addison Francisco on 10/11/16.
+//  Copyright © 2016 Addison Francisco. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+    func updateLabels() {
+        let bodyFont = UIFont.preferredFont(forTextStyle: .body)
+        nameLabel.font = bodyFont
+        valueLabel.font = bodyFont
+        
+        let caption1Font = UIFont.preferredFont(forTextStyle: .caption1)
+        serialNumberLabel.font = caption1Font
+    }
+}
